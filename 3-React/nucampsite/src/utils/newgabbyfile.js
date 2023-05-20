1a95efb3b6482d0ceab7a8624f21e34c5ff6097e
gabbyPassion <129009206+gabbyPassion@users.noreply.github.com>
2023-05-20 14:56:23 -0500

    style: greeting callback function

--- 3-React/nucampsite/src/utils/newgabbyfile.js
+++ 3-React/nucampsite/src/utils/newgabbyfile.js
@@ -1,1 +1,14 @@
 console.log("Hey Hunter");
+function greetIfHey(name, message) {
+        if (message.toLowerCase() === 'hey') {
+        return `Hello, ${name}!`;
+        } else {
+        return "Please say 'hey' first.";
+        }
+    }
+    
+    // Example usage
+    const userInput = prompt("Enter your message:");
+    const result = greetIfHey("Christopher Hunter", userInput);
+    console.log(result);
+    
