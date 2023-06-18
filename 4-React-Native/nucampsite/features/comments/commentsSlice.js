0b2ab9d51ec9d0b69c0f7fcacb97dd39199ae884
gabbyPassion <129009206+gabbyPassion@users.noreply.github.com>
2023-06-17 23:40:47 -0400

    Changes to React Native App
    Changes to React Native App

---
+++ 4-React-Native/nucampsite/features/comments/commentsSlice.js
@@ -0,0 +1,60 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from '../../shared/baseUrl';
+
+export const fetchComments = createAsyncThunk(
+    'comments/fetchComments',
+    async () => {
+        const response = await fetch(baseUrl + 'comments');
+        if (!response.ok) {
+            return Promise.reject(
+                'Unable to fetch, status: ' + response.status
+            );
+        }
+        const data = await response.json();
+        return data;
+    }
+);
+
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    async (comment, { dispatch, getState }) => {
+        const { comments } = getState();
+        const newComment = {
+            ...comment,
+            date: new Date().toISOString(),
+            id: comments.commentsArray.length
+        };
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        dispatch(addComment(newComment));
+    }
+);
+
+const commentsSlice = createSlice({
+    name: 'comments',
+    initialState: { isLoading: true, errMess: null, commentsArray: [] },
+    reducers: {
+        addComment: (state, action) => {
+            state.commentsArray.push(action.payload);
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchComments.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchComments.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.errMess = null;
+                state.commentsArray = action.payload;
+            })
+            .addCase(fetchComments.rejected, (state, action) => {
+                state.isLoading = false;
+                state.errMess = action.error
+                    ? action.error.message
+                    : 'Fetch failed';
+            });
+    }
+});
+
+export const { addComment } = commentsSlice.actions;
+export const commentsReducer = commentsSlice.reducer;
